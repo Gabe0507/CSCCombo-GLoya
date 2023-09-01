04e4be8bf2df860e0d750bcb2eee57161e12bebe
Loya <S02948614@ccc.ccofc.edu>
2023-09-01 16:20:35 -0600

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * This is a  to the hello world program it writes Hello World! to the console!
+ * Written by: Gabriel Loya
+ * Date: 9/1/23
+ / * 
+  
+  
+   
+   */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//Hello world one line comment
+		
+		 System.out.println("hello World!");
+		
+		System.out.println();
+		
+		int num1 = 66;
+		
+		int num2 = 12;
+		
+		int feet =  num1 / num2;
+		int inches = num1 % num2;
+		
+		System.out.println("The item is " + feet + "ft and " + inches + "in");
+	}
+
+}
