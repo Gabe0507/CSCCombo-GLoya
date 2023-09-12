074e8b9174e239e239ea8c0d413012b22aba37c7
Loya <S02948614@ccc.ccofc.edu>
2023-09-12 15:58:24 -0600

    whileLoop

---
+++ whileLoops/src/whileLoops/whileLoops.java
@@ -0,0 +1,63 @@
+package whileLoops;
+
+import java.util.Scanner;
+
+public class whileLoops {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+
+//		int i = 0, num1 = 4, num2 =1;
+//		
+//		while (i>5) {
+//			num2 = num2 * num1;
+//			System.out.println("i: " + i + ", next number :" + num2);
+//			i++;
+//			
+//		}
+//		System.out.println("good Bye!");
+		
+		
+		
+//		int currPower = 2;
+//		String userChar = "y";
+//		
+//		while (userChar.equals("y")) {
+//			System.out.println(currPower);
+//			currPower = currPower * 2;
+//			System.out.println("Wanna know next Power? y|n");
+//			
+//			userChar = scnr.nextLine();
+//		}
+//		System.out.println("good Bye!");
+		
+//		String name = "";
+//		int age = -1;
+//		System.out.println("Input your name: ");
+//		name = input.next();
+//		while (age < 0)
+//		{
+//			System.out.println("Input your age: ");
+//			age = input.nextInt();
+//		}
+//		System.out.println(name + " " + age);
+		
+//		int count = 3;
+//		while (count > 0)
+//		{
+//			System.out.println(count);
+//			count --;
+//		}
+//		System.out.println("Blast off~!");
+		
+//		int count = 5;
+//		while (count < 100)
+//		{
+//			System.out.println(count);
+//			count ++;
+//		}
+		
+		
+	}
+
+}
