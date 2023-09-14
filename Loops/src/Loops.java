c093151b37a0118ea5e930b28ff407c1cf2558f9
Loya <S02948614@ccc.ccofc.edu>
2023-09-14 16:11:44 -0600

    Loop

---
+++ Loops/src/Loops.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		int count = 0;
+		int total = 0;
+//		while (count < 10)
+//		{
+//			System.out.println(count);
+//			count ++;
+//			total += + count;
+//			
+//	}
+//		System.out.println("the total of count is " + total);
+		
+		for (count = 0; count <= 10; count++)
+		{
+			System.out.println(count);
+			total += + count;
+			
+	}
+		System.out.println("the total of count is " + total);
+
+	}
+
+}
