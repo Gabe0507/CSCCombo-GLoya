1409afeb5507f32127becd682708bc83af376ce4
Loya <S02948614@ccc.ccofc.edu>
2023-09-20 16:18:01 -0600

    Printing

---
+++ Printing/src/Printf.java
@@ -0,0 +1,32 @@
+
+public class Printf {
+
+	public static void main(String[] args) {
+		
+//		System.out.printf("Four thirds = %.3f", 4.0/3.0);
+//		System.out.printf("Four thirds = %-5.3f", 4.0/3.0);
+//		System.out.printf("Four thirds = %10.2f%10s ", 4.0/3.0," is the answer");
+//		System.out.printf("Four thirds = %010.2f%10s ", 4.0/3.0," is the answer");
+		
+//		String formattedStr = String.format("\nGrand total:%,.2f", 1000000.00);
+//		System.out.println(formattedStr);
+		
+//		int i;
+//		for (i = 0; i < 100; i++) {
+//			System.out.printf("%4s", i + 1);
+//			if (((i+1)%10==0) && (i != 0)) {
+//				System.out.println();
+//			}
+//			
+//		}
+		
+			
+			 
+				
+		
+			
+			
+		
+	}
+
+}
